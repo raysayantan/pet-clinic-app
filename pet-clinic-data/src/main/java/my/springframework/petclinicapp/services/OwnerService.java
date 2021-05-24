bc4394fe1142d73efd653b71287cc60bba32f8db
sayray <sayray@paypal.com>
2021-05-24 18:24:19 +0530

    Added Service interface

---
+++ pet-clinic-data/src/main/java/my/springframework/petclinicapp/services/OwnerService.java
@@ -0,0 +1,12 @@
+package my.springframework.petclinicapp.services;
+
+import my.springframework.petclinicapp.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/my/springframework/petclinicapp/services/VetService.java
@@ -0,0 +1,11 @@
+package my.springframework.petclinicapp.services;
+
+import my.springframework.petclinicapp.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
