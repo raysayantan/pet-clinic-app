59767b16834793365310e48a878dd607c18e990f
sayray <sayray@paypal.com>
2021-05-26 18:42:18 +0530

    Fixed the component scan

--- pet-clinic-web/src/main/java/my/springframework/Controllers/IndexController.java
+++ pet-clinic-web/src/main/java/my/springframework/Controllers/IndexController.java
@@ -3,10 +3,11 @@ package my.springframework.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping({""})
 @Controller
 public class IndexController {
 
-    @RequestMapping({"", "/", "index", "index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String index(){
         return "index";
     }
